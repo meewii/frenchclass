54c1f617e535a9c61017b896e223ea3c80f7d21b
meewii <ma.sinfreu@gmail.com>
2015-11-24 17:01:38 +0100

    Pronoms possessifs; gitignore

--- js/pronouns.js
+++ js/pronouns.js
@@ -1,4 +1,18 @@
-/*
 jQuery(function() {
-    $( document ).tooltip();
-});*/
+
+    $('tr.cells td').hover(
+        function() {
+            $( this ).css( {'color':'red' } );
+
+          /*  var idx = ($(this).index())+1;
+            $('table tr.rank td:nth-child('+idx+')').css( {'color':'orange' } );*/
+
+        }, function() {
+            $( this ).css( {'color':'white' } );
+
+           /* var idx = ($(this).index())+1;
+            $('table tr.rank td:nth-child('+idx+')').css( {'color':'white' } );*/
+        }
+    );
+
+});
