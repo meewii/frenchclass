f915273cdbc9f5cef0a8128855e0a2c30fcdc7dc
meewii <ma.sinfreu@gmail.com>
2015-11-24 17:59:10 +0100

    Colored pronouns in tooltips

--- js/pronouns.js
+++ js/pronouns.js
@@ -1,17 +1,19 @@
 jQuery(function() {
 
-    $('tr.cells td').hover(
+    var $cells = $('tr.cells td');
+
+    $cells.hover(
         function() {
             $( this ).addClass('hover');
-
-          /*  var idx = ($(this).index())+1;
-            $('table tr.rank td:nth-child('+idx+')').css( {'color':'orange' } );*/
-
         }, function() {
             $( this ).removeClass('hover');
-           /* var idx = ($(this).index())+1;
-            $('table tr.rank td:nth-child('+idx+')').css( {'color':'white' } );*/
         }
     );
+
+    var options = {
+        'html':true,
+        'container': 'body'
+    };
+    $cells.tooltip(options);
 
 });
